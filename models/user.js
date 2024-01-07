738604a8753ac892fc1069c1793d0c27fe78fd04
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-01-07 21:59:19 +0530

    add user schema
    hash passwords using mongoose pre-save hook

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const { createHmac, randomBytes } = require('node:crypto');
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "/images/default_profilePhoto"
+    },
+    role: {
+        type: String,
+        enum: ['USER', 'ADMIN'],
+        default: "USER"
+    }
+},
+    {timestamps: true}
+);
+
+userSchema.pre('save', function (next){
+    const user = this;
+
+    if(!userSchema.isModified(user.password)) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac('sha256', salt)
+                            .update(user.password)
+                            .digest('hex');
+    
+    user.salt = salt;
+    user.password = hashedPassword;
+
+    next();
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
