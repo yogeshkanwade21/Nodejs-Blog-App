ecbaa0aa3f33e925a078cc6a7b566e60b3165f6b
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-01-08 10:58:09 +0530

    feat: signup functionality

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const userRouter = require('./routes/user');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
 
-//route
-app.get('/', (req, res) => {
+// Middleware to parse the request body
+app.use(express.urlencoded({ extended: false }));
+
+// connect to the database
+mongoose.connect('mongodb://127.0.0.1:27017/blog-app')
+    .then(() => {
+        console.log('mongodb connection established');
+    })
+    .catch(err => console.log(err.message));
+
+// Home route
+app.get('/home', (req, res) => {
     return res.render('home');
 })
+
+// user routes
+app.use('/user', userRouter);
 
 // server configuration
 const server = app.listen(PORT, ()=> {
