11ad7d902192773fd66105eef8c47ccb1ff73648
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-01-08 22:32:08 +0530

    add jwt auth functions

---
+++ services/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const secretPhrase = 'secretphraseforsecrettokenforsecrettoken';
+
+function createTokenForUser(user) {
+    const payload = {
+        _id : user._id,
+        fullName : user.fullName,
+        email : user.email
+    }
+
+    const token = jwt.sign(payload, secretPhrase);
+    return token;
+}
+
+function verifyToken(token) {
+
+    try {
+        const payload = jwt.verify(token, secretPhrase,(err, user)=> {
+            console.log("token verified", payload);
+            return payload;
+        });
+    } catch (error) {
+        console.error('Token verification failed:', err.message);
+    }
+}
+
+module.exports = {createTokenForUser, verifyToken};
