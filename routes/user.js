e63dda287cde266510a061dc58229a315dcd8bae
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-01-08 10:56:31 +0530

    add user router

---
+++ routes/user.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const User = require('../models/user');
+const router = Router();
+
+router.get('/signin', (req, res) => {
+    return res.render('signin');
+})
+
+router.get('/signup', (req, res) => {
+    return res.render('signup');
+})
+
+router.post('/signin', (req, res) => {
+})
+
+router.post('/signup', async (req, res) => {
+    const { fullName, email, password } = req.body;
+    await User.create({
+        fullName,
+        email,
+        password,
+    });
+
+    return res.redirect('/home');
+})
+
+module.exports = router;
