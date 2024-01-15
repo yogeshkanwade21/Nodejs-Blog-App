7535489863a234838453028254447586ece45b9f
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-01-15 15:09:17 +0530

    feat: profile page
    list user blogs on profile

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const User = require('../models/user');
+const Blog = require('../models/blog');
 const router = Router();
 
 router.get('/signin', (req, res) => {
@@ -40,6 +41,29 @@ router.post('/signup', async (req, res) => {
 
 router.get('/logout', (req, res) => {
     res.clearCookie("token").redirect('/home');
+})
+
+router.get('/profile', async (req, res) => {
+    if(req.user){
+        const profileUser = await User.findOne({ _id: req.user._id});
+        const blogs = await Blog.find({ createdBy: profileUser._id}).sort({'createdAt': -1});
+        const profileBlogs = blogs.map((blog, index) => (
+            {
+                number: index + 1,
+                title: blog.title,
+                createdAt: blog.createdAt,
+                _id: blog._id
+            }
+        ));
+        // console.log('dynamicBlogs here', profileBlogs);
+        return res.render('profile', {
+            user: profileUser,
+            blogs: profileBlogs
+    })
+    } else {
+        res.redirect('/home');
+    }
+
 })
 
 module.exports = router;
